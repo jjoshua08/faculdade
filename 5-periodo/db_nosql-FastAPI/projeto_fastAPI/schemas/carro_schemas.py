09c7fcf03e8c01c6e77e0380d8ea6fc1483ce271
Jonathan <tratar.quitacao_0d@icloud.com>
2026-04-08 18:04:30 -0300

    Atualização

--- 5-periodo/db_nosql-FastAPI/projeto_fastAPI/schemas/carro_schemas.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-class Carro(BaseModel):
-    nome: str
-    marca: str
-    placa: str
-    ano: int
-    cor: str
--- 5-periodo/db_nosql-FastAPI/projeto_fastAPI/schemas/users_schemas.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-class User(BaseModel):
-    nome: str
-    email: str
-    idade: int
-
